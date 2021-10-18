de2c3e3e60c463ba74263669100ad852ef00a706
Karan Dua <karandua2002@gmail.com>
2021-10-18 20:46:46 +0530

    third commit

--- pages/components/Category.js
+++ pages/components/Category.js
@@ -1,16 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Category() {
     return (
-        <div className="flex justify-center items-center m-3 p-2 border-2 border-porabay rounded-2xl transition duration-500 ease-in-out transform hover:scale-110">
-            <div className="p-3">
-                <Image
-                    src="/art.png"
-                    width={150}
-                    height={150}
-                />
-            </div>
+        <div>
+            <Link href="/subcategories">
+                <div className="flex justify-center items-center m-3 p-2 border-2 border-porabay rounded-2xl transition duration-500 ease-in-out transform hover:scale-110 cursor-pointer">
+                    <div className="p-3">
+                        <Image
+                            src="/art.png"
+                            width={150}
+                            height={150}
+                        />
+                    </div>
+                </div>
+            </Link>
+            
         </div>
+        
         
     )
 }
--- pages/subcategories.js
+++ pages/subcategories.js
@@ -2,6 +2,7 @@ function subcategories() {
     return (
         <div>
             Congo !! u entered a new page
+            U will see All products of that category under respective sub Categories
         </div>
     )
 }
