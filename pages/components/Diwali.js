4abc4c525f485373ea624dc836df6a201b3acfdf
Karan Dua <karandua2002@gmail.com>
2021-10-26 02:56:00 +0530

    most UI done

--- pages/components/Diwali.js
+++ pages/components/Diwali.js
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react";
 import ProductIcon from "./ProductIcon"
 
 function Diwali({prod}) {
+    const [name, setName] = useState('name');
+    
+    useEffect(() => {
+        if(prod[0] !== ""){
+            setName(prod[0]);
+        }
+    },[]);
     return (
         <div className="flex w-full justify center items-center px-3 m-1  rounded-sm hover:bg-gray-100 ">
             <div className="flex flex-col w-full">
                 <div className="flex">
-                    <p className="m-2 text-lg font-bold">{prod[0]}:</p>
+                    <p className="m-2 text-lg font-bold">{name}:</p>
                 </div>
                 <div className="flex overflow-x-scroll">
                     {prod.map((product, index) => {
