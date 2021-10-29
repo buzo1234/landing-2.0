b4ce61a165b5dc64b1381abac640dd88e0206373
Karan Dua <karandua2002@gmail.com>
2021-10-29 23:25:05 +0530

    home icon

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -13,21 +13,21 @@ function Header() {
     },[cartstate])
     return (
         <div className="shadow-md sticky top-0 z-50 bg-white">
-            <div className="flex items-center justify-between bg-white py-4 px-4 z-50 max-w-6xl mx-auto">
+            <div className="flex items-center justify-between bg-white py-5 px-2 z-50 max-w-5xl mx-auto">
                 <Link href="/">
                     <div className="flex items-center cursor-pointer">
                         
                         <div className="font-serif items-center px-1 text-2xl"><b>Local&#x0915;&#x092E;&#x093E;&#x0932;</b></div>
                     </div>
                 </Link>
-                <div className="flex items-center space-x-4">
+                <div className="flex items-center justify-center">
                     <Link href="/">
                         <HomeIcon className="w-12" />
                     </Link>
                     <Link href="/cartarea">
-                        <div className="relative w-full cursor-pointer">
-                            <div className="absolute -mt-1 ml-7 justify-center bg-red-400 items-center text-xs text-center rounded-full w-4">{subqty}</div>
-                            <ShoppingCartIcon className="w-10 mr-4"/>
+                        <div className="relative w-full cursor-pointer ">
+                            <div className="absolute -mt-1 ml-9 justify-center bg-red-400 items-center text-xs text-center rounded-full w-4">{subqty}</div>
+                            <ShoppingCartIcon className="w-8 ml-4 -mr-2"/>
                         </div>
                     </Link>
                     
